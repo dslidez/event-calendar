bfe143e77d4072b500e97b960be2df7ac0300e66
Daniil <danya.zinovenko@mail.ru>
2022-03-05 23:58:04 +0300

    routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
 
+import React from 'react';
+import Event from '../pages/Event';
+import Login from '../pages/Login';
+
 export interface IRoute {
-    
+    path: string;
+    component: React.ComponentType;
+    exact?: boolean;
+
 }
 
-export const routes = [
+//all routes by NAMES
+export enum RouteNames {
+    LOGIN = '/login',
+    EVENT = '/'
+}
 
+export const publickRoutes: IRoute[] = [
+    {path: RouteNames.LOGIN, exact: true, component: Login}
+]
+
+export const privateRoutes: IRoute[] = [
+    {path: RouteNames.EVENT, exact: true, component: Event}
 ]
